eaf54b197ccc5ab1e3be146eb81b546b05cb1c23
SnehasishBanik1609 <rana2013.banik@gmail.com>
2024-09-15 20:37:46 +0530

    Adding code base
    Adding code base for portfolio version 2024.1

---
+++ script.js
@@ -0,0 +1,82 @@
+// Navbar toggle functions
+function hamburg(){
+    const navbar = document.querySelector(".dropdown");
+    navbar.style.transform = "translateY(0px)";
+}
+
+function cancel(){
+    const navbar = document.querySelector(".dropdown");
+    navbar.style.transform = "translateY(-500px)";
+}
+
+// Typewriter Effect
+const texts = [
+    "SDET",
+    "Test Automation Engineer",
+    "Quality Assurance Engineer"
+];
+
+let speed = 100;
+const textElements = document.querySelector(".typewriter-text");
+
+let textIndex = 0;
+let charcterIndex = 0;
+
+function typeWriter(){
+    if (charcterIndex < texts[textIndex].length){
+        textElements.innerHTML += texts[textIndex].charAt(charcterIndex);
+        charcterIndex++;
+        setTimeout(typeWriter, speed); 
+    }
+    else{
+        setTimeout(eraseText, 1000);
+    }
+}
+
+function eraseText(){
+    if(textElements.innerHTML.length > 0){
+        textElements.innerHTML = textElements.innerHTML.slice(0,-1);
+        setTimeout(eraseText, 50);
+    }
+    else{
+        textIndex = (textIndex + 1) % texts.length;
+        charcterIndex = 0;
+        setTimeout(typeWriter, 500);
+    }
+}
+
+window.onload = typeWriter;
+
+
+// Feedback Image Shuffling
+const feedbackImages = [
+    document.getElementById('feedbackImage1'),
+    document.getElementById('feedbackImage2'),
+    document.getElementById('feedbackImage3'),
+    document.getElementById('feedbackImage4'),
+    document.getElementById('feedbackImage5'),
+    document.getElementById('feedbackImage6')
+];
+
+let currentImageIndex = 0;
+
+function shuffleFeedbackImages() {
+    // Hide all images
+    feedbackImages.forEach(image => {
+        image.style.display = 'none';
+    });
+
+    // Show the next image in the sequence
+    currentImageIndex = (currentImageIndex + 1) % feedbackImages.length;
+    feedbackImages[currentImageIndex].style.display = 'block';
+}
+
+// Shuffles the images every 5 seconds
+setInterval(shuffleFeedbackImages, 5000);
+
+// Navigate to contact page
+function navigateToContact() {
+    window.location.href = 'contact.html';
+}
+
+
